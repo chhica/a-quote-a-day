4f18ec2df8ce5fe6d0b775321fb94ee3b932fc26
Gee <chhica@live.com>
2017-12-16 21:34:14 -0800

    Made all changes

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express'); //requiring express module
+var app = express();  
+
+app.get('/', function(req,res)   //call back in each request incoming to the server
+{	
+	res.sendFile(__dirname+ '/index.html');//_dirname current dir 
+											// in which app.js is located
+});
+
+var port = process.env.port || 8080;//3000 ;//port number where server listens
+
+//app.listen(port);
+var server = app.listen(port, function(req, res)
+{
+	console.log("Catch the action at http://localhost:"+port);
+});
+
+
+
